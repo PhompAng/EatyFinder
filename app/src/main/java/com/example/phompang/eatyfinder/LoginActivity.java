5068c9272249134aa06e8672009a1ea66b2f49de
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-10-25 14:45:20 +0700

    update web client id

--- app/src/main/java/com/example/phompang/eatyfinder/LoginActivity.java
+++ app/src/main/java/com/example/phompang/eatyfinder/LoginActivity.java
@@ -116,7 +116,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         };
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken(getString(R.string.web_client_id))
                 .requestEmail()
                 .build();
 
