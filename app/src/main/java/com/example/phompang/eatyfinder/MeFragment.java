0d776127bbf1444e8fd64f00f359d65b9f90c98f
Pichai Sivawat <tingtong003tomy@gmail.com>
2016-12-05 16:58:01 +0700

    rename layout

--- app/src/main/java/com/example/phompang/eatyfinder/MeFragment.java
+++ app/src/main/java/com/example/phompang/eatyfinder/MeFragment.java
@@ -99,7 +99,7 @@ public class MeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_me, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
         ButterKnife.bind(this, v);
 //
 //        collapsingToolbarLayout = (CollapsingToolbarLayout) v.findViewById(R.id.collapsingToolbarLayout);
